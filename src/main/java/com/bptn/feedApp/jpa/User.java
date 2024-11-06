9b52b9076a22464ad6c18cc05258b3bc2838f815
el-johns <okpara.charles@gmail.com>
2024-11-06 16:13:26 -0500

    Added entity classes - Feed, FeedMetaData, Profile, User - into the jpa pkg and FeedRepository into the repository pkg

--- src/main/java/com/bptn/feedApp/jpa/User.java
+++ src/main/java/com/bptn/feedApp/jpa/User.java
@@ -14,7 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="user", schema = "public")
+@Table(name="\"user\"", schema = "public")
 public class User implements Serializable {   
 	private static final long serialVersionUID = 1L;
 	@Id
