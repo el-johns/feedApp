d2677d29557ebfecad2a9d79c166c01e505f8c74
Charles Okpara <118974592+el-johns@users.noreply.github.com>
2024-11-13 11:51:40 -0500

    updating the repository with the frameworks, libraries and dependecies required for the application

--- Desktop/EclipseProjects/feedApp/src/main/java/com/bptn/feedApp/controller/FeedController.java
+++ Desktop/EclipseProjects/feedApp/src/main/java/com/bptn/feedApp/controller/FeedController.java
@@ -4,17 +4,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bptn.feedApp.domain.PageResponse;
 import com.bptn.feedApp.jpa.Feed;
 import com.bptn.feedApp.service.FeedService;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/feeds")
+@RequestMapping("/feed")
 public class FeedController {
 
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -28,6 +31,22 @@ public class FeedController {
 		logger.debug("Creating Feed");
 
 		return this.feedService.createFeed(feed);
+	}
+
+	@GetMapping("/{feedId}")
+	public Feed getFeed(@PathVariable int feedId) {
+
+		logger.debug("Getting Feed, feedId: {}", feedId);
+
+		return this.feedService.getFeedById(feedId);
+	}
+
+	@GetMapping("/user/{pageNum}/{pageSize}")
+	public PageResponse<Feed> getUserFeeds(@PathVariable int pageNum, @PathVariable int pageSize) {
+
+		logger.debug("Getting User Feeds List, pageNum: {}, pageSize: {}", pageNum, pageSize);
+
+		return this.feedService.getUserFeeds(pageNum, pageSize);
 	}
 
 }
